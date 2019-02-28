455b28d7d5495aef0389cec80acf70a42c283f8e
Corey Roth <coreyroth@Coreys-MacBook.local>
2019-02-28 11:08:01 -0500

    Added instructions.

--- src/app/services/Constants.ts
+++ src/app/services/Constants.ts
@@ -1,4 +1,5 @@
 export class Constants {
     // replace with your own apiKey - this one has been revoked
+    // you should not normally store an apiKey in a client application - instead build your own servie to proxy the call
     public static apiKey: string = 'c00a3cef57a64dfe936de43dfc2369fe';
 }
